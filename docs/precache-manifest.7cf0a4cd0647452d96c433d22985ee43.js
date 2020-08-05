8ec53a657b7ca27489fd2afbb241cdad102b0066
Ricardo Israel Lozano Holcombe <israel.holcombe@icloud.com>
2020-08-05 12:54:13 -0500

    Add docs path

---
+++ docs/precache-manifest.7cf0a4cd0647452d96c433d22985ee43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59f2f465dbeac4767391e9b2de1e327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "367ea3579a648517b8e0",
+    "url": "/static/css/main.e4c31e4c.chunk.css"
+  },
+  {
+    "revision": "f9234fcf84c080de371a",
+    "url": "/static/js/2.982de7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.982de7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367ea3579a648517b8e0",
+    "url": "/static/js/main.391c8167.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
